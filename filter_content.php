a1cda8ac25f86aba3ed5153fa67a391812b12af5
James Thorne <james@thorne.bz>
2012-07-08 18:25:33 +0100

    fixed att to content

--- filter_content.php
+++ filter_content.php
@@ -1,12 +1,11 @@
 <?php
 
 
-function filter_the_content()
+function filter_the_content($content)
 {
 	if(is_single())
 	{
-		
-		return $content;
+		return "<div id='addtotimeline'>x</div>".$content;
 	}
 	
 	return $content;
